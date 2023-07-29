a5dbc593c77414f3cc658562047cfa4de200f032
johnie261 <njorogejohn261@gmail.com>
2023-07-29 23:28:23 +0300

    Create and deploy contracts to eth blockchain

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/vQg3rMqSgERp7OWHZPrGYoHs5BWPSHwb',
+      accounts: [
+        'e7cdcc8a8106f1b5f858635f26fb27b6e2cb54b7c8203afb0fc87dce74c29535'
+      ],
+     }
+  }
 };
